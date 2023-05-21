e6815a5f2f5fe96de9304e947e4eb606d6648d52
ddungiii <ggcc503@gmail.com>
2023-05-21 15:32:01 +0900

    937.reorder-data-in-log-files #1 (65/65)

--- problems/937.reorder-data-in-log-files.py
+++ problems/937.reorder-data-in-log-files.py
@@ -9,18 +9,16 @@
 
 class Solution:
     def reorderLogFiles(self, logs: list[str]) -> list[str]:
-        letters = []
-        digits = []
+        logs.sort(
+            key=lambda s: (
+                -s.split()[1].isalpha(),
+                s.split()[1:] if s.split()[1].isalpha() else 0,
+                s.split()[0] if s.split()[1].isalpha() else 0,
+            )
+        )
 
-        for log in logs:
-            if log.split()[1].isalpha():
-                letters.append(log)
-            else:
-                digits.append(log)
-
-        letters.sort(key=lambda log: (log.split()[1:], log.split()[0]))
-
-        return letters + digits
+        # logs.sort(key=lambda s: s.split()[1])
+        return logs
 
 
 # @lc code=end

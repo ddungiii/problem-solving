ccb929a83ce4a70d1312a5db1fb293d584c5a38f
ddungiii <ggcc503@gmail.com>
2023-06-03 17:53:40 +0900

    [fix: timeout by removing regular expression] 5.longest-palindromic-substring

--- problems/5.longest-palindromic-substring.py
+++ problems/5.longest-palindromic-substring.py
@@ -13,7 +13,6 @@ import time
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         def isPalindrome(s: str) -> bool:
-            s = re.sub("[^a-z0-9]", "", s)
             return s == s[::-1]
 
         if len(s) < 2 or s == s[::-1]:
@@ -21,21 +20,17 @@ class Solution:
 
         substr = s[0]
 
-        for i in range(len(s)):
-            # print(f"i: {i}")
-            # print(f"substr: {substr}")
+        for i in range(0, len(s) - 1):
             for j in range(i + len(substr), len(s)):
-                # print(f"  j: {j}")
                 if isPalindrome(s[i : j + 1]):
                     substr = s[i : j + 1]
 
-        # print(f"result: {substr}")
         return substr
 
     # @lc code=end
 
 
-soluion = Solution()
-start = time.time()
-print(soluion.longestPalindrome(input("input: ")))
-print(f"time: {time.time() - start:.4f}s ")
+# soluion = Solution()
+# start = time.time()
+# print(soluion.longestPalindrome(input("input: ")))
+# print(f"time: {time.time() - start:.4f}s ")

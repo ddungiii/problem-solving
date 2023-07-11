bb29bc1c1c80e1b55074e036cb42b26e1694f34a
ddungiii <ggcc503@gmail.com>
2023-07-12 00:08:38 +0900

    15.3-sum
    - two pointers15.3-sum

--- problems/15.3-sum.py
+++ problems/15.3-sum.py
@@ -1,5 +1,6 @@
 #
 # @lc app=leetcode id=15 lang=python3
+
 #
 # [15] 3Sum
 #
@@ -15,14 +16,21 @@ class Solution:
             # for removing duplicates
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            for j in range(i + 1, len(nums) - 1):
-                if j > i + 1 and nums[j] == nums[j - 1]:
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                # for removing duplicates
+                if left > i + 1 and nums[left] == nums[left - 1]:
+                    left += 1
                     continue
-                for k in range(j + 1, len(nums)):
-                    if k > j + 1 and nums[k] == nums[k - 1]:
-                        continue
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        results.append([nums[i], nums[j], nums[k]])
+
+                sum = nums[i] + nums[left] + nums[right]
+                if sum == 0:
+                    results.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                elif sum < 0:
+                    left += 1
+                elif sum > 0:
+                    right -= 1
 
         return results
 

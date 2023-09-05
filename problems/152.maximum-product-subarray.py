6c686f3599d73bf9121347d89183c2751f7101e0
ddungiii <ggcc503@gmail.com>
2023-09-06 01:35:35 +0900

    152.maximum-product-subarray
    - store min, max product both
    - swap if element is negative
    - refer to chatGPT

--- problems/152.maximum-product-subarray.py
+++ problems/152.maximum-product-subarray.py
@@ -12,61 +12,20 @@ import sys
 
 class Solution:
     def maxProduct(self, nums: list[int]) -> int:
-        max = reduce(lambda a, c: a * c, nums, 1)
+        if len(nums) == 0:
+            return 0
 
-        left, right = 0, len(nums) - 1
+        min_product, max_product, result = nums[0], nums[0], nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] < 0:
+                min_product, max_product = max_product, min_product
 
-        while left < right and right >= 0 and left <= len(nums) - 1:
-            candidates = []
-            # 0. l
-            candidates.append(max / nums[left] if nums[left] != 0 else -sys.maxsize)
+            min_product = min(min_product * nums[i], nums[i])
+            max_product = max(max_product * nums[i], nums[i])
 
-            # 1. r
-            candidates.append(max / nums[right] if nums[right] != 0 else -sys.maxsize)
+            result = max(max_product, result)
 
-            # 2. l, r
-            candidates.append(
-                max / (nums[left] * nums[right])
-                if nums[left] != 0 and nums[right] != 0 and left + 1 < right
-                else -sys.maxsize
-            )
-
-            # 3. l, l+1
-            candidates.append(
-                max / (nums[left] * nums[left + 1])
-                if nums[left] != 0 and nums[left + 1] != 0 and left + 1 < right
-                else -sys.maxsize
-            )
-
-            # 4. r, r-1
-            candidates.append(
-                max / (nums[right] * nums[right - 1])
-                if nums[right] != 0 and nums[right - 1] != 0 and left + 1 < right
-                else -sys.maxsize
-            )
-
-            index = -1
-            for i, candidate in enumerate(candidates):
-                if candidate > max:
-                    max = candidate
-                    index = i
-
-            if index == 0:
-                left += 1
-            elif index == 1:
-                right -= 1
-            elif index == 2:
-                left += 1
-                right -= 1
-            elif index == 3:
-                left += 2
-            elif index == 4:
-                right -= 2
-            else:
-                left += 2
-                right -= 2
-
-        return int(max)
+        return result
 
 
 # @lc code=end

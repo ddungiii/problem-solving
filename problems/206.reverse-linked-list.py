de90bb5e95801591771668ea7b8868fca77c43bc
ddungiii <ggcc503@gmail.com>
2023-09-08 22:31:00 +0900

    206.reverse-linked-list
    - use iterative

--- problems/206.reverse-linked-list.py
+++ problems/206.reverse-linked-list.py
@@ -18,21 +18,13 @@ from typing import Optional
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
+        node, prev = head, None
 
-        list = []
-        while head:
-            list.append(head.val)
-            head = head.next
-        list.reverse()
+        while node:
+            next, node.next = node.next, prev
+            prev, node = node, next
 
-        head_reversed = node = ListNode(list[0])
-        for i in range(1, len(list)):
-            n = ListNode(list[i])
-            node.next, node = n, n
-
-        return head_reversed
+        return prev
 
 
 # @lc code=end

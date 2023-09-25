409946807a0c7fc0fba9ba69b1b884e49c685d67
ddungiii <ggcc503@gmail.com>
2023-09-25 21:25:05 +0900

    200.number-of-islands (fail)

---
+++ problems/200.number-of-islands.py
@@ -0,0 +1,55 @@
+#
+# @lc app=leetcode id=200 lang=python3
+#
+# [200] Number of Islands
+#
+from typing import List
+
+# @lc code=start
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m, n = len(grid), len(grid[0])
+        
+        class Point:
+            def __init__(self, x, y) -> None:
+                self.x = x
+                self.y = y 
+
+        def get_neighbors(point: Point):
+            x, y = point.x, point.y
+
+            xs, ys = [x, x+1] if x < n-2 else [x], [y, y+1] if y < m-2 else [y]
+
+            neighbors = []
+            for new_x in xs:
+                for new_y in ys:
+                    neighbors.append(Point(new_x, new_y))
+
+
+            return neighbors
+
+        
+        discoverd = [[0 for _ in range(m)] for _ in range(n)]
+        
+        def dfs(num_islands: int, point: Point = Point(0, 0)):
+            x, y = point.x, point.y
+            discoverd[x][y] = 1
+
+            if grid[x][y] == 1 and (x > 0 and grid[x-1][y] == 0) and (y > 0 and grid[x][y-1] == 0):
+                print("new!")
+                num_islands += 1
+
+            print(f"grid[{x}][{y}]: {grid[x][y]} nums: {num_islands}")
+            neighbors = get_neighbors(point)
+            for neighbor in neighbors:
+                print(f"new_x: {neighbor.x}, new_y: {neighbor.y}")
+                if discoverd[neighbor.x][neighbor.y] == 0:
+                    num_islands = max(num_islands, dfs(num_islands, neighbor))
+
+            return num_islands   
+        
+        num_islands = dfs(0) 
+
+        return num_islands
+        
+# @lc code=end

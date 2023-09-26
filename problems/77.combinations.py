a73cd3c470eb13eff98083482b80ee14bd1fbe2a
ddungiii <ggcc503@gmail.com>
2023-09-26 19:27:52 +0900

    77.combinations
    - use dfs

--- problems/77.combinations.py
+++ problems/77.combinations.py
@@ -10,7 +10,24 @@ from itertools import combinations
 # @lc code=start
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return [list(comb) for comb in combinations(range(1, n + 1), k)]
+        nums = list(range(1, n + 1))
+
+        def dfs(parent: List[int], child: List[int] = []):
+            if len(child) == k:
+                result.append(child)
+                return
+
+            for i, element in enumerate(parent):
+                new_parent = parent[i + 1 :]
+                new_child = child[:]
+
+                new_child.append(element)
+                dfs(new_parent, new_child)
+
+        result: List[List[int]] = []
+        dfs(nums)
+
+        return result
 
 
 # @lc code=end

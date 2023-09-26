59260bfef1a0352d784dde291715e2eda2ade277
ddungiii <ggcc503@gmail.com>
2023-09-26 13:51:58 +0900

    200.number-of-islands

--- problems/200.number-of-islands.py
+++ problems/200.number-of-islands.py
@@ -2,58 +2,35 @@
 # @lc app=leetcode id=200 lang=python3
 #
 # [200] Number of Islands
-#
-from dis import disco
+#s
 from typing import List
 
 # @lc code=start
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        n, m = len(grid), len(grid[0])
-        print(f"n: {n}, m: {m}")
+        def dfs(i, j):
+            if (i < 0 or i > len(grid) -1) \
+                or (j < 0 or j > len(grid[0]) -1) \
+                or grid[i][j] != "1":
+                return
+            
+            grid[i][j] = "0"
+
+            dfs(i-1, j)
+            dfs(i, j-1)
+            dfs(i+1, j)
+            dfs(i, j+1)
+
+
+        num_islands = 0
         
-        class Point:
-            def __init__(self, x, y) -> None:
-                self.x = x
-                self.y = y 
-
-        def get_neighbors(point: Point):
-            x, y = point.x, point.y
-
-            xs, ys = [x, x+1] if x < n-1 else [x], [y, y+1] if y < m-1 else [y]
-
-            neighbors = []
-            for new_x in xs:
-                for new_y in ys:
-                    neighbors.append(Point(new_x, new_y))
-
-
-            return neighbors
-
-        
-        discoverd = [[0 for _ in range(m)] for _ in range(n)]
-        
-        def dfs(num_islands: int, point: Point = Point(0, 0)):
-            x, y = point.x, point.y
-            discoverd[x][y] = 1
-            if num_islands == 0 and grid[x][y] == "1":
-                num_islands += 1
-
-            if grid[x][y] == "1" and (x > 0 and grid[x-1][y] == "0") and (y > 0 and grid[x][y-1] == "0"):
-                print("new!")
-                num_islands += 1
-
-            print(f"grid[{x}][{y}]: {grid[x][y]} nums: {num_islands}")
-            neighbors = get_neighbors(point)
-            for neighbor in neighbors:
-                print(f"new_x: {neighbor.x}, new_y: {neighbor.y}")
-                if discoverd[neighbor.x][neighbor.y] == 0:
-                    num_islands = max(num_islands, dfs(num_islands, neighbor))
-
-            return num_islands   
-        
-        num_islands = dfs(0) 
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    dfs(i, j)
+                    num_islands += 1
 
         return num_islands
         
+
 # @lc code=end

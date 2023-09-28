30385985069ffe127aa88791661c6330feaaf673
ddungiii <ggcc503@gmail.com>
2023-09-29 01:01:24 +0900

    332.reconstruct-itinerary (80/81)
        - fix the way that return result

--- problems/332.reconstruct-itinerary.py
+++ problems/332.reconstruct-itinerary.py
@@ -3,31 +3,28 @@
 #
 # [332] Reconstruct Itinerary
 #
-from imp import init_frozen
-from socket import TIPC_HIGH_IMPORTANCE
 from typing import List
 # @lc code=start
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        tickets.sort(key=lambda ticket: (ticket[0], ticket[1]))
+        tickets.sort(key=lambda ticket: [ticket[0], ticket[1]])
 
-        def dfs(index: int, tickets: List[List[str]], itinerary: List[str]):
+        def dfs(tickets: List[List[str]], itinerary: List[str]):
             if not len(tickets):
                 return itinerary
             
             last = itinerary[-1]
             
-            result=itinerary
             for i, ticket in enumerate(tickets):
                 if ticket[0] == last:
                     new_tickets = tickets[:]
-                    new_tickets.remove(ticket)
-                    result = dfs(i+1, new_tickets, itinerary + [ticket[1]])
-
-            return result
+                    new_tickets.pop(i)
+                    result = dfs(new_tickets, itinerary + [ticket[1]])
+                    if result:
+                        return result
         
         
-        return dfs(0, tickets, ["JFK"])
+        return dfs(tickets, ["JFK"])
 
         
 # @lc code=end

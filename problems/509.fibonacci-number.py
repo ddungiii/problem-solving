c2887d87673c21ac67ad036e75b18592b922879e
ddungiii <ggcc503@gmail.com>
2023-09-29 23:24:09 +0900

    509.fibonacci-number
    - use memoization with dp
    - 509ms -> 47ms

--- problems/509.fibonacci-number.py
+++ problems/509.fibonacci-number.py
@@ -3,15 +3,22 @@
 #
 # [509] Fibonacci Number
 #
+from collections import defaultdict
 
 
 # @lc code=start
 class Solution:
+    dp = defaultdict(int)
+
     def fib(self, n: int) -> int:
         if n <= 1:
             return n
 
-        return self.fib(n - 1) + self.fib(n - 2)
+        if self.dp[n]:
+            return self.dp[n]
+        self.dp[n] = self.fib(n - 1) + self.fib(n - 2)
+
+        return self.dp[n]
 
 
 # @lc code=end

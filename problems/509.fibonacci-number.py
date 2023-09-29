8aaec72a78a8cfc27ac1b47639d29a3b9389223f
ddungiii <ggcc503@gmail.com>
2023-09-29 23:27:36 +0900

    508.fibonacci-number
    - use tabulation

--- problems/509.fibonacci-number.py
+++ problems/509.fibonacci-number.py
@@ -11,12 +11,11 @@ class Solution:
     dp = defaultdict(int)
 
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
+        self.dp[0] = 0
+        self.dp[1] = 1
 
-        if self.dp[n]:
-            return self.dp[n]
-        self.dp[n] = self.fib(n - 1) + self.fib(n - 2)
+        for i in range(2, n + 1):
+            self.dp[i] = self.dp[i - 1] + self.dp[i - 2]
 
         return self.dp[n]
 

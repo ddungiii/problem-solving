a8aa20ea2be18c96af979b0de2a14c48a602d63f
ddungiii <ggcc503@gmail.com>
2023-09-29 23:30:31 +0900

    509.fibonacci-number
    - optimize space complexity

--- problems/509.fibonacci-number.py
+++ problems/509.fibonacci-number.py
@@ -3,21 +3,16 @@
 #
 # [509] Fibonacci Number
 #
-from collections import defaultdict
 
 
 # @lc code=start
 class Solution:
-    dp = defaultdict(int)
-
     def fib(self, n: int) -> int:
-        self.dp[0] = 0
-        self.dp[1] = 1
+        x, y = 0, 1
+        for i in range(n):
+            x, y = y, x + y
 
-        for i in range(2, n + 1):
-            self.dp[i] = self.dp[i - 1] + self.dp[i - 2]
-
-        return self.dp[n]
+        return x
 
 
 # @lc code=end

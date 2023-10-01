838ea9e87646516133f8389456d66151cec4cf2b
ddungiii <ggcc503@gmail.com>
2023-10-01 20:00:29 +0900

    70.climbing-stairs

---
+++ problems/70.climbing-stairs.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=70 lang=python3
+#
+# [70] Climbing Stairs
+#
+import collections
+# @lc code=start
+class Solution:
+    dp = collections.defaultdict(int)
+
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        
+        self.dp[1] = 1
+        self.dp[2] = 2
+
+        
+        for i in range(3, n + 1):
+            self.dp[i] = self.dp[i-1] + self.dp[i-2]
+        
+        return self.dp[n]
+        
+# @lc code=end
+

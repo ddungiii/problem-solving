8593e9eccd26af63bdf8e903ab6d617597e822f6
ddungiii <ggcc503@gmail.com>
2023-10-08 02:37:37 +0900

    boj: 17070.py (timeout)
    - fix to not use class but timeout

--- boj/problems/17070.py
+++ boj/problems/17070.py
@@ -7,76 +7,50 @@ from enum import Enum
 
 Status = Enum("Status", ["RIGHT", "DOWN", "DIAGONAL"])
 
-
-class Pipe:
-    """
-    (0,0) (0,1) (0,2)
-    (1,0) (1,1) (1,2)
-    (2,0) (2,1) (2,2)
-    """
-
-    def __init__(self, x=0, y=1, status=Status.RIGHT):
-        self.x = x
-        self.y = y
-        self.status = status
-
-    def get(self):
-        return self.x, self.y
-
-    def moves(self):
-        moves = []
-        if self.status == Status.RIGHT:
-            if self.y + 1 < n and not walls[self.x][self.y + 1]:
-                moves.append(Pipe(self.x, self.y + 1, Status.RIGHT))
-                if (
-                    self.x + 1 < n
-                    and not walls[self.x + 1][self.y]
-                    and not walls[self.x + 1][self.y + 1]
-                ):
-                    moves.append(Pipe(self.x + 1, self.y + 1, Status.DIAGONAL))
-
-        elif self.status == Status.DOWN:
-            if self.x + 1 < n and not walls[self.x + 1][self.y]:
-                moves.append(Pipe(self.x + 1, self.y, Status.DOWN))
-                if (
-                    self.y + 1 < n
-                    and not walls[self.x][self.y + 1]
-                    and not walls[self.x + 1][self.y + 1]
-                ):
-                    moves.append(Pipe(self.x + 1, self.y + 1, Status.DIAGONAL))
-
-        elif self.status == Status.DIAGONAL:
-            if self.y + 1 < n and not walls[self.x][self.y + 1]:
-                moves.append(Pipe(self.x, self.y + 1, Status.RIGHT))
-            if self.x + 1 < n and not walls[self.x + 1][self.y]:
-                moves.append(Pipe(self.x + 1, self.y, Status.DOWN))
-                if (
-                    self.y + 1 < n
-                    and not walls[self.x][self.y + 1]
-                    and not walls[self.x + 1][self.y + 1]
-                ):
-                    moves.append(Pipe(self.x + 1, self.y + 1, Status.DIAGONAL))
-
-        return moves
-
-
 n = int(input())
 walls = []
 for _ in range(n):
     walls.append(list(map(int, input().split())))
 
 
-def dfs(pipe=Pipe()):
-    x, y = pipe.get()
+def get_moves(x, y, status):
+    moves = []
+    if status == Status.RIGHT:
+        if y + 1 < n and not walls[x][y + 1]:
+            moves.append((x, y + 1, Status.RIGHT))
+            if x + 1 < n and not walls[x + 1][y] and not walls[x + 1][y + 1]:
+                moves.append((x + 1, y + 1, Status.DIAGONAL))
+
+    elif status == Status.DOWN:
+        if x + 1 < n and not walls[x + 1][y]:
+            moves.append((x + 1, y, Status.DOWN))
+            if y + 1 < n and not walls[x][y + 1] and not walls[x + 1][y + 1]:
+                moves.append((x + 1, y + 1, Status.DIAGONAL))
+
+    else:
+        if y + 1 < n and not walls[x][y + 1]:
+            moves.append((x, y + 1, Status.RIGHT))
+        if x + 1 < n and not walls[x + 1][y]:
+            moves.append((x + 1, y, Status.DOWN))
+            if y + 1 < n and not walls[x][y + 1] and not walls[x + 1][y + 1]:
+                moves.append((x + 1, y + 1, Status.DIAGONAL))
+
+    return moves
+
+
+def dfs(x, y, status):
     if (x, y) == (n - 1, n - 1):
         return 1
 
     count = 0
-    moves = pipe.moves()
-    for move in moves:
-        count += dfs(move)
+    moves = get_moves(x, y, status)
+    if not len(moves):
+        return count
+
+    for new_x, new_y, new_status in moves:
+        count += dfs(new_x, new_y, new_status)
 
     return count
 
 
-print(dfs())
+print(dfs(0, 1, Status.RIGHT))

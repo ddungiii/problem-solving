96ec6821375e232062ee4de3cad7655f93fc9f02
ddungiii <ggcc503@gmail.com>
2023-10-08 22:33:32 +0900

    boj: 17281.py (80% timeout)
    - optimize variable not to use list

--- boj/problems/17281.py
+++ boj/problems/17281.py
@@ -1,38 +1,44 @@
 """
 https://www.acmicpc.net/problem/17281
+
+9
+1 2 4 3 0 2 1 0 3
+1 2 1 2 0 0 0 0 1
+3 4 2 3 1 2 3 4 0
+0 1 2 3 4 2 1 0 0
+0 0 0 0 0 0 1 4 4
+0 4 0 4 0 4 0 4 0
+0 4 2 2 2 2 2 2 2
+1 1 1 1 1 1 1 1 0
+0 2 0 3 0 1 0 2 0
 """
 from itertools import permutations
 from collections import deque
+import time
 
 
 def play_baseball(line_up, index, inning):
     out = 0
     score = 0
 
-    base = deque([0, 0, 0])
+    b1, b2, b3 = 0, 0, 0
     while out < 3:
         player = hits[inning][line_up[index - 1]]
 
         if player == 0:
             out += 1
         elif player == 1:
-            base.appendleft(1)
-            score += base.pop()
+            score += b3
+            b1, b2, b3 = 1, b1, b2
         elif player == 2:
-            base.appendleft(1)
-            base.appendleft(0)
-            score += base.pop()
-            score += base.pop()
+            score += b2 + b3
+            b1, b2, b3 = 0, 1, b1
         elif player == 3:
-            base.appendleft(1)
-            base.appendleft(0)
-            base.appendleft(0)
-            score += base.pop()
-            score += base.pop()
-            score += base.pop()
+            score += b1 + b2 + b3
+            b1, b2, b3 = 0, 0, 1
         elif player == 4:
-            score += sum(base) + 1
-            base = deque([0, 0, 0])
+            score += 1 + b1 + b2 + b3
+            b1, b2, b3 = 0, 0, 0
 
         index += 1
         index %= 9
@@ -52,12 +58,17 @@ def get_score(line_up):
 
 
 def solution():
+    # start = time.time()
+
     answer = 0
 
     for comb in permutations(range(1, 9), 8):
-        line_up = list(comb)[0:3] + [0] + list(comb)[3:8]
+        line_up = deque(comb)
+        line_up.insert(3, 0)
 
         answer = max(answer, get_score(line_up))
+
+    # print(f"time: {time.time() - start:.4f}s")
 
     return answer
 

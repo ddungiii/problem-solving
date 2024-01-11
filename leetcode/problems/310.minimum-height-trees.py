c630f306696286d05f59d18be940e5837f26a261
ddungiii <ggcc503@gmail.com>
2024-01-11 20:39:37 +0900

    310.minimum-height-trees.py

--- leetcode/problems/310.minimum-height-trees.py
+++ leetcode/problems/310.minimum-height-trees.py
@@ -12,32 +12,8 @@ import collections
 
 class Solution:
     def findMinHeightTrees(self, n: int, edges: list[list[int]]) -> list[int]:
-        def make_tree(root):
-            tree = collections.defaultdict(list)
-            visited = set([root])
-
-            queue = [root]
-            while queue:
-                node = queue.pop()
-
-                neighbors = graph[node]
-                for neighbor in neighbors:
-                    if not neighbor in visited:
-                        tree[node].append(neighbor)
-                        queue.append(neighbor)
-                        visited.add(neighbor)
-
-            return tree
-
-        def get_height(tree, root, depth):
-            heights = [depth]
-            for node in tree[root]:
-                heights.append(get_height(tree, node, depth + 1))
-
-            return max(heights)
-
-        # Preprocessing
-        heights = []
+        if not edges:
+            return [0]
 
         graph = collections.defaultdict(list)
         for edge in edges:
@@ -45,19 +21,25 @@ class Solution:
             graph[u].append(v)
             graph[v].append(u)
 
-        # Get height
+        leaves = []
         for i in range(n):
-            tree = make_tree(i)
-            height = get_height(tree, i, 0)
-            heights.append((i, height))
+            if len(graph[i]) == 1:
+                leaves.append(i)
 
-        # Get min heights
-        heights.sort(key=lambda x: x[1])
-        min_height = heights[0][1]
+        while n > 2:
+            n -= len(leaves)
 
-        heights = [node for node, h in heights if h == min_height]
+            new_leaves = []
+            for leaf in leaves:
+                node = graph[leaf].pop()
+                graph[node].remove(leaf)
 
-        return heights
+                if len(graph[node]) == 1:
+                    new_leaves.append(node)
+
+            leaves = new_leaves
+
+        return leaves
 
 
 # @lc code=end

c79ebccc797d9cb272eb9e8b31304de9afa09260
ddungiii <ggcc503@gmail.com>
2024-01-15 14:32:55 +0900

    leetcode: 1038.binary-search-tree-to-greater-sum-tree.py

---
+++ leetcode/problems/1038.binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=1038 lang=python3
+#
+# [1038] Binary Search Tree to Greater Sum Tree
+#
+
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        def dfs(node, acc):
+            """
+            큰 숫자 방향으로 DFS
+            """
+            if not node:
+                return acc
+
+            acc = dfs(node.right, acc)
+            acc += node.val
+            node.val = acc
+            acc = dfs(node.left, acc)
+
+            return acc
+
+        dfs(root, 0)
+        return root
+
+
+# @lc code=end

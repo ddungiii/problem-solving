8099d3611c51f0eae146978f600e535209280cb9
ddungiii <ggcc503@gmail.com>
2024-03-05 22:35:01 +0900

    programmers: 비밀지도.java
    - use replaceAll

--- programmers/level1/java/비밀지도.java
+++ programmers/level1/java/비밀지도.java
@@ -1,52 +1,18 @@
 package java;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        ArrayList<String> answer = new ArrayList<String>();
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            result[i] = String.format("%16s", result[i]);
+            result[i] = result[i].substring(16 - n);
 
-        for (int i = 0; i < arr1.length; i++) {
-            HashMap<Integer, Boolean> b1 = this.intToBinary(n, arr1[i]);
-            HashMap<Integer, Boolean> b2 = this.intToBinary(n, arr2[i]);
-
-            String merged = this.mergeBinaries(b1, b2, n);
-            answer.add(merged);
+            result[i] = result[i].replaceAll("1", "#");
+            result[i] = result[i].replaceAll("0", " ");
         }
 
-        return answer.toArray(new String[0]);
+        return result;
     }
 
-    private HashMap<Integer, Boolean> intToBinary(int n, int map) {
-        HashMap<Integer, Boolean> binary = new HashMap<>();
-
-        String binaryString = Integer.toBinaryString(map);
-        for (int i = 0; i < binaryString.length(); i++) {
-            char bit = binaryString.charAt(binaryString.length() - i - 1);
-            int bitInt = Integer.parseInt(String.valueOf(bit));
-            if (bitInt > 0) {
-                binary.put(i, true);
-            }
-        }
-
-        return binary;
-    }
-
-    private String mergeBinaries(HashMap<Integer, Boolean> b1, HashMap<Integer, Boolean> b2, int n) {
-        StringBuilder merged = new StringBuilder();
-
-        for (int i = n - 1; i >= 0; i--) {
-            boolean n1 = b1.getOrDefault(i, false);
-            boolean n2 = b2.getOrDefault(i, false);
-
-            if (n1 || n2) {
-                merged.append("#");
-            } else {
-                merged.append(" ");
-            }
-        }
-
-        return merged.toString();
-    }
 }

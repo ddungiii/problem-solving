8fcd931dcd03fc3a2d9b29027ca90a81eb8c3e88
ddungiii <ggcc503@gmail.com>
2024-03-07 16:28:18 +0900

    programmers: 불량_사용자.java
    - use DFS

--- programmers/level3/java/불량_사용자.java
+++ programmers/level3/java/불량_사용자.java
@@ -1,27 +1,31 @@
 package programmers.level3.java;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
+    Set<String> set = new HashSet<>();
+
     public int solution(String[] user_id, String[] banned_id) {
-        ArrayDeque<ArrayList<String>> candidates = new ArrayDeque<>();
-        // 1. for banned_id, find user_id using regex
-        for (String ban : banned_id) {
-            ArrayList<String> candidate = new ArrayList<String>();
+        dfs(user_id, banned_id, 0, "");
+        return set.size();
+    }
 
-            for (String user : user_id) {
-                if (isCandidate(user, ban)) {
-                    candidate.add(user);
-                }
-            }
-
-            candidates.add(candidate);
+    private void dfs(String[] user_id, String[] banned_id, int idx, String str) {
+        if (idx == banned_id.length) {
+            // base condition
+            char[] c = str.toCharArray();
+            Arrays.sort(c);
+            set.add(new String(c));
+            return;
         }
 
-        // 2. get combination numbers.
-        return this.getPossibleCases(candidates, new ArrayList<>());
+        for (int i = 0; i < user_id.length; i++) {
+            if (this.isCandidate(user_id[i], banned_id[idx]) && !str.contains(i + "")) {
+                dfs(user_id, banned_id, idx + 1, str + i);
+            }
+        }
     }
 
     private boolean isCandidate(String user, String ban) {
@@ -29,38 +33,4 @@ class Solution {
         return user.matches(ban);
     }
 
-    private int getPossibleCases(ArrayDeque<ArrayList<String>> cur, ArrayList<String> acc) {
-        System.out.print(cur);
-        System.out.println(acc);
-        if (cur.size() == 0) {
-            return 1;
-        }
-        int total = 0;
-        for (ArrayList<String> list : cur) {
-            total += list.size();
-        }
-        if (total == 0) {
-            return 1;
-        }
-
-        int result = 0;
-        ArrayList<String> target = cur.pop();
-
-        for (String name : target) {
-            ArrayDeque<ArrayList<String>> new_cur = new ArrayDeque<>(cur);
-            for (List<String> list : new_cur) {
-                list.remove(name);
-            }
-
-            ArrayList<String> new_acc = new ArrayList<>(acc);
-            if (new_acc.contains(name)) {
-                continue;
-            }
-
-            new_acc.add(name);
-            result += this.getPossibleCases(new_cur, new_acc);
-        }
-
-        return result;
-    }
 }

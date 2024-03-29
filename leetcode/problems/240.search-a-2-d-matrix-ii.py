4e5176e8c55ffa92b185191496534078225d72d2
ddungiii <ggcc503@gmail.com>
2024-03-29 13:44:47 +0900

    leetcode: 240.search-a-2-d-matrix-ii
    - use binary search in row, column
    - 156ms

--- leetcode/problems/240.search-a-2-d-matrix-ii.py
+++ leetcode/problems/240.search-a-2-d-matrix-ii.py
@@ -8,7 +8,19 @@
 # @lc code=start
 class Solution:
     def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
-        return any(target in row for row in matrix)
+        N, M = len(matrix), len(matrix[0])
+
+        r, c = 0, M - 1
+
+        while r < N and c >= 0:
+            if matrix[r][c] < target:
+                r += 1
+            elif matrix[r][c] > target:
+                c -= 1
+            else:
+                return True
+
+        return False
 
 
 # @lc code=end

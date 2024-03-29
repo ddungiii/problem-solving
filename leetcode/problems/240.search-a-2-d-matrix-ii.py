89f1c31f0aeeb913af67d2e1946716fe5f6c5bf0
ddungiii <ggcc503@gmail.com>
2024-03-29 13:41:03 +0900

    leetcode: 240.search-a-2-d-matrix-ii
    - use any
    - 132ms

--- leetcode/problems/240.search-a-2-d-matrix-ii.py
+++ leetcode/problems/240.search-a-2-d-matrix-ii.py
@@ -6,22 +6,9 @@
 
 
 # @lc code=start
-import bisect
-
-
 class Solution:
     def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
-        def binary_search(l):
-            i = bisect.bisect_left(l, target, hi=M - 1)
-            return l[i] == target
-
-        N, M = len(matrix), len(matrix[0])
-
-        for r in range(N):
-            if binary_search(matrix[r]):
-                return True
-
-        return False
+        return any(target in row for row in matrix)
 
 
 # @lc code=end

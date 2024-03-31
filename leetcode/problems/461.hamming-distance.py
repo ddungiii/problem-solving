59ba1007de13fd296fb9cfdca33dc2076936bee2
ddungiii <ggcc503@gmail.com>
2024-03-31 16:51:44 +0900

    leetcode: 461.hamming-distance
    - use bin, count

--- leetcode/problems/461.hamming-distance.py
+++ leetcode/problems/461.hamming-distance.py
@@ -8,17 +8,7 @@
 # @lc code=start
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        def count_bits(number):
-            cnt = 0
-            while number > 0:
-                if number % 2 == 1:
-                    cnt += 1
-
-                number = number >> 1
-
-            return cnt
-
-        return count_bits(x ^ y)
+        return bin(x ^ y).count("1")
 
 
 # @lc code=end
